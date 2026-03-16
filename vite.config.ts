824488a13b7ef1d89254aa6ed403e92d74b238ec
akvarij <aljaz.resnik@gmail.com>
2026-03-16 07:37:38 +0100

    Change vite config to outDir docs for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/onboarding-flow-assignment/",
+  build: {
+    outDir: "docs",
+  },
   plugins: [react()],
 });
